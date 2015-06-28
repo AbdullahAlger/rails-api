98192f1dc4ec259f74857115b127e41f578b000c
Abdullah Alger <abdullahalger@me.com>
2015-06-28 13:09:26 +0200

    got rid of some files and code in serializer

--- app/serializers/list_serializer.rb
+++ app/serializers/list_serializer.rb
@@ -1,15 +1,3 @@
 class ListSerializer < ActiveModel::Serializer
   attributes :id, :name, :permissions, :user_id
-
-  def name
-    object.name
-  end
-
-  def permissions
-    object.permissions
-  end
-
-  def user_id
-    object.user_id
-  end
 end
