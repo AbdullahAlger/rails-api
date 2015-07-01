4ccd356d326346a3868721074bbfc0729e252d83
Abdullah Alger <abdullahalger@me.com>
2015-07-01 23:37:33 +0200

    changed more controller code

--- db/migrate/20150623100235_create_lists.rb
+++ db/migrate/20150623100235_create_lists.rb
@@ -3,6 +3,7 @@ class CreateLists < ActiveRecord::Migration
     create_table :lists do |t|
       t.string :name
       t.string :permissions
+      t.references :user, index:true
 
       t.timestamps null: false
     end
