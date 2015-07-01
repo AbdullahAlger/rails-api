0cfd0e1eaf54ae1ddb3978149accd15a09da6c1d
Abdullah Alger <abdullahalger@me.com>
2015-07-01 23:57:00 +0200

    .

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -15,7 +15,7 @@ class Api::ListsController < ApiController
   end
 
   def create
-    list = current_user.lists.new(list_params)
+    list = current_user.lists.build(list_params)
 
     if list.save
       render json: list
