55d447b3be004e5a722a8f49ea208455620a6ee4
Abdullah Alger <abdullahalger@me.com>
2015-07-02 23:49:38 +0200

    trying cors again

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,6 +3,14 @@ class ApiController < ApplicationController
   skip_before_action :verify_authenticity_token
   attr_reader :current_user
 
+  before_filter :allow_ajax_request_from_other_domains
+
+  def allow_ajax_request_from_other_domains
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = '*'
+  end
+
   private
 
   def authenticated?
