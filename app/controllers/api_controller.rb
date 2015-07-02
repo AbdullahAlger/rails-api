ad429ff61de85b7b0819748fec73294f5e45a8e3
Abdullah Alger <abdullahalger@me.com>
2015-07-02 23:41:20 +0200

    trying again

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,30 +3,6 @@ class ApiController < ApplicationController
   skip_before_action :verify_authenticity_token
   attr_reader :current_user
 
-  before_filter :cors_preflight_check
-  after_filter :cors_set_access_control_headers
-
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Max-Age'] = "1728000"
-  end
-
-  def cors_preflight_check
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version'
-    headers['Access-Control-Max-Age'] = '1728000'
-  end
-
-  def index
-    @users = User.all
-
-    respond_to do |format|
-      format.json { render :json => @users }
-    end
-  end
-
   private
 
   def authenticated?
